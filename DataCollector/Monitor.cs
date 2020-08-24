536e6262075df07d68d5ec588269b9b0683fc4bd
poroh-kun <meta.poroh.kun@gmail.com>
2020-08-25 01:59:02 +0300

    windows part begin

---
+++ DataCollector/Monitor.cs
@@ -0,0 +1,40 @@
+﻿using OpenHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector
+{
+    public class Monitor
+    {
+        Computer computer;
+
+        public Monitor()
+        {
+            computer = new Computer() { CPUEnabled = true, GPUEnabled = true };
+            computer.Open();
+        }
+
+        public void Refresh()
+        {
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                hardware.Update();
+
+                Console.WriteLine("{0}: {1}", hardware.HardwareType, hardware.Name);
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    // Celsius is default unit
+                    if (sensor.SensorType == SensorType.Temperature)
+                    {
+                        Console.WriteLine("{0}: {1}°C", sensor.Name, sensor.Value);
+                        // Console.WriteLine("{0}: {1}°F", sensor.Name, sensor.Value*1.8+32);
+                    }
+
+                }
+            }
+        }
+    }
+}
