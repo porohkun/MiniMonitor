cdf353dc82fc722f5a35656d99d33e2dbc550a50
Poroh-kun <poroh.kun@gmail.com>
2022-05-23 21:29:06 +0300

    working line

--- MiniMonitor/MiniMonitor.ino
+++ MiniMonitor/MiniMonitor.ino
@@ -33,6 +33,8 @@ ModbusinoSlave Modbus(1);
 #define ZERO 0x00
 #define CPU_MIN_TEMP 40
 #define CPU_MAX_TEMP 60
+#define GPU_MIN_TEMP 40
+#define GPU_MAX_TEMP 60
 
 #define CPU_LABEL "CPU"
 #define GPU_LABEL "GPU"
@@ -54,8 +56,9 @@ uint8_t _regSize = sizeof(_registers) / sizeof(_registers[0]);
 
 bool _cpuMode = true;
 uint8_t _cpuLine[22]{ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO};
-uint8_t _cpuLineSize = sizeof(_cpuLine) / sizeof(_cpuLine[0]);
-uint8_t _cpuIndex = 0;
+uint8_t _gpuLine[22]{ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO, ZERO};
+uint8_t _lineSize = sizeof(_cpuLine) / sizeof(_cpuLine[0]);
+uint8_t _lineIndex = 0;
 
 void setup()
 {
@@ -72,7 +75,11 @@ void setup()
 	u8g2.begin();
 
 	Modbus.setup(115200);
-	//Serial.begin(115200);
+}
+
+uint16_t clamp(uint16_t x, uint16_t lower, uint16_t upper)
+{
+	return min(upper, max(x, lower));
 }
 
 void loop()
@@ -81,38 +88,25 @@ void loop()
 	if (Button.GetDown(MODE_BUTTON))
 		_cpuMode = !_cpuMode;
 
-	// if(Serial)
 	Modbus.loop(_registers, 11);
 	Timer.Loop();
-
-
-	if (Timer.IsRaised)
-	{
-		Serial.println(millis()/1000);
-	}
-
-	if (Timer.IsRaised)
-	{
-		int16_t t = max(0, _registers[REG_CPU_TEMP] - CPU_MIN_TEMP) * 16;
-		_cpuLine[_cpuIndex] = min(16, max(1, t / (CPU_MAX_TEMP - CPU_MIN_TEMP)));
-		_cpuIndex = (_cpuIndex + 1) % _cpuLineSize;
-		//_registers[REG_CPU_TEMP] = (_registers[REG_CPU_TEMP] + 1) % 99;
-	}
 
 	_registers[0] = ID_0;
 	_registers[1] = ID_1;
 	_registers[2] = ID_2;
 
+	if (Timer.IsRaised)
+	{
+		int16_t tcpu = clamp(_registers[REG_CPU_TEMP], CPU_MIN_TEMP, CPU_MAX_TEMP);
+		int16_t tgpu = clamp(_registers[REG_GPU_TEMP], GPU_MIN_TEMP, GPU_MAX_TEMP);
+		_cpuLine[_lineIndex] = (tcpu - CPU_MIN_TEMP) * 16 / (CPU_MAX_TEMP - CPU_MIN_TEMP);
+		_gpuLine[_lineIndex] = (tgpu - GPU_MIN_TEMP) * 16 / (GPU_MAX_TEMP - GPU_MIN_TEMP);
+		_lineIndex = (_lineIndex + 1) % _lineSize;
+	}
+
 	u8g2.clearBuffer();
 
 	u8g2.setColorIndex(1);
-
-	// char clocktext[6] = "00:00";
-	// setNumberToCharBuffer(_registers[REG_HOUR], 0, clocktext);
-	// setNumberToCharBuffer(_registers[REG_MINUTE], 3, clocktext);
-
-	// u8g2.setFont(u8g2_font_logisoso34_tn);
-	// u8g2.drawStr(0, 64 - (64 - 16 - 34) / 2, clocktext);
 
 	drawSideTemp(CPU_LABEL, _registers[REG_CPU_TEMP], 25);
 	drawSideTemp(GPU_LABEL, _registers[REG_GPU_TEMP], 49);
@@ -124,22 +118,28 @@ void loop()
 	if (Serial.available())
 		u8g2.drawStr(0, 35, "ser aval");
 
-    u8g2.drawStr(75, 25, "E");
+	u8g2.drawStr(75, 26, "E");
 
 	char temp[3] = "00";
-	setNumberToCharBuffer((millis()/1000)%100, 0, temp);
+	setNumberToCharBuffer((millis() / 1000) % 100, 0, temp);
 	u8g2.drawStr(0, 45, temp);
 
+	setNumberToCharBuffer(_lineIndex, 0, temp);
+	u8g2.drawStr(30, 45, temp);
+
+	setNumberToCharBuffer(_gpuLine[_lineIndex], 0, temp);
+	u8g2.drawStr(60, 45, temp);
+
 	setNumberToCharBuffer(_registers[REG_HOUR], 0, temp);
-	u8g2.drawStr(0, 65, temp);
+	u8g2.drawStr(0, 64, temp);
 
 	setNumberToCharBuffer(_registers[REG_MINUTE], 0, temp);
-	u8g2.drawStr(45, 65, temp);
+	u8g2.drawStr(30, 64, temp);
 
-	for (uint8_t i = 0; i < _cpuLineSize; i++)
+	for (uint8_t i = 0; i < _lineSize; i++)
 	{
-		uint8_t height = _cpuLine[(_cpuIndex + i) % _cpuLineSize];
-		u8g2.drawBox(128 - (_cpuLineSize - i) * 5 + 1, 16 - height, 4, height);
+		uint8_t height = min(1, (_cpuMode ? _cpuLine : _gpuLine)[(_lineIndex + i) % _lineSize]);
+		u8g2.drawBox(128 - (_lineSize - i) * 5 + 1, 16 - height, 4, height);
 	}
 
 	u8g2.sendBuffer();
@@ -180,9 +180,9 @@ void itoa(int n, char s[], uint16_t base)
 		n = -n;			/* делаем n положительным числом */
 	i = 0;
 	do
-	{						   /* генерируем цифры в обратном порядке */
+	{							 /* генерируем цифры в обратном порядке */
 		s[i++] = n % base + '0'; /* берем следующую цифру */
-	} while ((n /= base) > 0);   /* удаляем */
+	} while ((n /= base) > 0);	 /* удаляем */
 	if (sign < 0)
 		s[i++] = '-';
 	s[i] = '\0';
