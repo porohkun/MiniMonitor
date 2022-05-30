5a7336bd144867531a06b13e70d3bf44d5fa78f6
Poroh-kun <poroh.kun@gmail.com>
2022-05-30 19:34:51 +0300

    working time

--- font/tall_s.c
+++ font/tall_s.c
@@ -4,7 +4,7 @@
   Glyphs: 4/256
   BBX Build Mode: 0
 */
-const uint8_t tall[70] U8G2_FONT_SECTION("tall") = 
+const uint8_t tall_s[70] U8G2_FONT_SECTION("tall") = 
   "\4\0\3\2\3\3\1\2\4\5\7\0\376\5\376\5\376\0\0\0\0\0)C\12}<K&\266%\13"
   "\0G\12}<K&\226\266d\1P\13}\34C\222\331\6%\14\1U\10}\34\231o\311\2\0\0"
   "\0\4\377\377\0";
